f54d28fa4e64fd8408b1156b4bfdc0426ff675f7
suman1611 <sumanwork25@gmail.com>
2022-04-05 16:27:01 +0530

    ,

---
+++ crud-react-redux/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Postform from './Postform';
+import Allpost from './Allpost';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Postform/>
+      <Allpost/>
+    </div>
+  );
+}
+
+export default App;
